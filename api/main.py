49d0bc0fc200a2d0e7d43b208fc6e7ff0efbcf8b
Jay <jp6631@srmist.edu.in>
2025-05-25 00:09:11 +0530

    Building website using fastAPI

--- api/main.py
+++ api/main.py
@@ -4,38 +4,50 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
-
-
+from starlette.middleware.cors import CORSMiddleware
 
 # When you go to http://localhost:8000 in your browser,
 # you're trying to connect to a web server that should be running on your own computer, listening on port 8000.
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 MODEL = tf.keras.models.load_model("../saved_models/1")
-CLASS_NAMES = ["Early Blight","Late Blight","Healthy"]
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
+def read_file_as_image(data: bytes) -> np.ndarray:
+    image = Image.open(BytesIO(data)).convert("RGB")    # Open and convert to RGB
+    image = image.resize((256, 256))
+    return np.array(image)
 
-def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
-    return image
 @app.post("/predict")
-async def predict(
-        file: UploadFile = File(...)
-):
-    image = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image,0)
+async def predict(file: UploadFile = File(...)):
+    image = read_file_as_image(await file.read())  # Read and preprocess image
+    img_batch = np.expand_dims(image, axis=0) # Add batch dimension: (1, height, width, channels)
 
     prediction = MODEL.predict(img_batch)
 
     predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
-    confidence = np.max(prediction[0])
+    confidence = float(np.max(prediction[0]))
 
     return {
         'class': predicted_class,
-        'confidence' : float(confidence)
+        'confidence': confidence
     }
+
 if __name__ == "__main__":
-    uvicorn.run(app,host='localhost',port=8000)
+    uvicorn.run(app, host='localhost', port=8000)
     # Runs the app using Uvicorn, a fast ASGI server.
     # Binds the server to localhost on port 8000.
